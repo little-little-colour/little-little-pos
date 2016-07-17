e42949a79c0a9d0986bc62256918de95f8b7a992
ipoplar <ipoplarwhite@gmail.com>
2016-07-17 21:27:28 +0800

    achieve buildReceipt

--- src/main.js
+++ src/main.js
@@ -96,9 +96,102 @@ function buildReceiptItems(cartItems, promotions) {
   return receiptItems;
 }
 
+/*
+构建“买二赠一”的打折商品
+*/
+function promotionItems(receiptItems) {
+  var promotionItems = [];
+  var promotions = loadPromotions();
+
+  receiptItems.forEach(function (receiptItem) {
+    var cartItems = receiptItem.cartItem;
+
+    var promotionType = findPromotionType(cartItems.item.barcode, promotions);
+
+    if (promotionType == 'BUY_TWO_GET_ONE_FREE') {
+      var saveCount = parseInt(cartItems.count / 3);
+
+      promotionItems.push({
+        name: cartItems.item.name,
+        unit: cartItems.item.unit,
+        saveCount: saveCount,
+        promotionType: promotionType
+      });
+    }
+  });
+
+  return promotionItems;
+}
+
+/*
+计算所有商品的总价和节省价
+*/
+function buildReceipt(receiptItems) {
+  var receipt;
+  var total = 0;
+  var savedTotal = 0;
+
+  receiptItems.forEach(function (receiptItem) {
+    total += receiptItem.subTotal;
+    savedTotal += receiptItem.saved;
+  });
+  receipt = {receiptItem: receiptItems, savedTotal: savedTotal, total: total};
+
+  return receipt;
+}
+
+/*
+  构建小票基本信息
+*/
+function buildReceiptText(receipt, receiptItems) {
+
+  return '***<没钱赚商店>收据***\n' + text(receipt) +  (promotionsText(receiptItems) || '') +'----------------------\n' +
+    '总计：' + formatPrice(receipt.total) + '(元)\n' + '节省：' + formatPrice(receipt.savedTotal) + '(元)\n' + '**********************';
+}
+
+/*
+  构建小票商品信息
+*/
+function text(receipt) {
+  var text = '';
+  var receiptItems = receipt.receiptItem;
+
+  receiptItems.forEach(function (receiptItem) {
+    var cartItem = receiptItem.cartItem;
+    text += '名称：' + cartItem.item.name + '，数量：' + receiptItem.cartItem.count + cartItem.item.unit + '，单价：' + formatPrice(cartItem.item.price) + '(元)，小计：' + formatPrice(receiptItem.subTotal) + '(元)\n';
+  });
+
+  return text;
+}
+
+/*
+“买二赠一”的优惠信息
+*/
+function promotionsText(receiptItems) {
+  var text = '';
+  var title = '';
+  var promotions = promotionItems(receiptItems);
+
+  promotions.forEach(function (promotion) {
+    text += '名称：' + promotion.name + '，数量：' + promotion.saveCount + promotion.unit + '\n';
+    title = (promotion.promotionType == 'BUY_TWO_GET_ONE_FREE')?('----------------------\n买二赠一商品：\n') : '';
+  });
+  text = title + text;
+
+  return text;
+}
+
+/*
+  价格输出格式化
+*/
+function formatPrice(price) {
+
+  return price.toFixed(2);
+}
 
 
 module.exports = {
   buildCartItems:buildCartItems,
-  buildReceiptItems:buildReceiptItems
+  buildReceiptItems:buildReceiptItems,
+  buildReceipt:buildReceipt
 };
